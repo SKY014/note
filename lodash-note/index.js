9d1ac0291c9f41c83df442c9bcb44e7d46b0954b
sky014 <884433774@qq.com>
2018-09-03 21:18:46 +0800

    “lodash

--- lodash-note/index.js
+++ lodash-note/index.js
@@ -4,3 +4,41 @@
  * @version 1.0
  * 
  * */ 
+
+/**
+ * @name Array方法 methods
+ * 
+ * */ 
+
+let arr=[1,2,3,4,5,6,7,8];
+let arr1=[0,1,false,2,'',3];
+
+
+/**
+ * chunk(大块) 返回一个新的数组
+ * params1 array 被加工的数组
+ * params2 每一块的长度
+ * 
+ * 
+ * */ 
+
+let chunk_arr=_.chunk(arr,2);
+// console.log(chunk_arr);
+
+/**
+ * compact 使简洁 返回一个新的过滤过的数组
+ * params1 arr 被简洁的数组
+ * 
+ * */ 
+
+let compact_arr=_.compact(arr1);
+console.log(compact_arr);
+
+/**
+ * concat
+ * 
+ * */ 
+
+
+
+
