1cf5a5fa4802a662c3d8c242191622b6476c69b1
sky014 <884433774@qq.com>
2018-09-27 21:09:41 +0800

    “代码提交

---
+++ knowledgeBase-note/layui-note/index.js
@@ -0,0 +1,40 @@
+/**
+ * @name kayui框架笔记
+ * 
+ * 底层方法
+ * 全局配置
+ * 定义模块
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */ 
+
+/**
+ * @name 内置模块
+ * 
+ * 弹出层 layer   
+ * 日期和时间选择 laydate
+ * 即时通讯 layim
+ * 分页 laypage
+ * 模板引擎 laytpl
+ * 
+ * 数据表格 table
+ * 
+ * 
+ * 表单 form
+ * 文件上传 upload
+ * 常用元素操作 element
+ * 颜色选择器 colorpicker
+ * 滑块 slider
+ * 评分 rate
+ * 轮播 carousel
+ * 流加载 flow
+ * 工具集 util
+ * 代码修饰器 code
+ *  
+ * 
+ * 
+ * 
+ * */  
---
+++ knowledgeBase-note/readme.md
@@ -0,0 +1,7 @@
+#知识库项目笔记
+##主体框架layui jquery q.js
+
+
+# 核心框架  layui     核心框架	https://www.layui.com/ 
+# 路由框架  	q.js（纯js轻量级路由框架）https://github.com/itorr/q.js
+# mvvm框架	pandyle.js（专为jquery编写的mvvm库）https://gitee.com/pandarrr/pandyle
