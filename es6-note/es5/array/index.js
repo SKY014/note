914a4599fa66273f1a45af473bf3a523e94ee4e6
sky014 <884433774@qq.com>
2018-10-16 14:33:02 +0800

    “数组常用方法

--- es6-note/es5/array/index.js
+++ es6-note/es5/array/index.js
@@ -7,5 +7,8 @@ var arr=[10,20,'30','xyz'];
 // indexOf 搜索一个指定元素的位置
 console.log(arr.indexOf(10));
 
-// slice 截取array的部分元素 然后返回一个新的array 两个参数
+// slice 截取array的部分元素 然后返回一个新的array 两个参数 param1 从某个位置开始
+// param1 到某个索引结束 但不包括该索引 [param1,param2);
+
+var arr1 =[]
 
